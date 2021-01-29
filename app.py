d156fb6ef8c38ef0fb92e91fcc1e99dac87d177e
Kevin Tsai <b2778173@hotmail.com>
2021-01-29 17:57:08 +0800

    mongoDB connection setting

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-import requests
 from resources.stock import Stock
 from resources.stock import News
 from resources.stock import CompanyNews
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
 api = Api(app)
-
+client = MongoClient(host='127.0.0.1', port=27017)
+db = client.demo
 
 api.add_resource(Stock, '/stock')
 api.add_resource(News, '/news')
