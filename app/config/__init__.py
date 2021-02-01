14350da684ea0cebfaba6ae7c65606f9de8a8285
Kevin Tsai <b2778173@hotmail.com>
2021-02-01 11:29:46 +0800

    hide sensetive info

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,4 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
 class config:
-    FINNHUB_BASE_URL = "https://finnhub.io/api/v1"
-    API_KEY = "c07nu3v48v6retjanc0g"
-    MONGODB_CONNECTION_STRING = "mongodb://testapidb:XDKcnIL9dBEUVPecyyCeTPvdrtsuPMK1PFXQTe6iQx9FaoPzFqAgrWPUl9umCYfU3XStuTGXXBlY1oQyIFy7Mg%3D%3D@testapidb.mongo.cosmos.azure.com:10255/test?authSource=admin&replicaSet=globaldb&maxIdleTimeMS=120000&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true"
+
+    FINNHUB_BASE_URL = os.getenv("FINNHUB_BASE_URL")
+    API_KEY = os.getenv("API_KEY")
+    MONGODB_CONNECTION_STRING = os.getenv("MONGODB_CONNECTION_STRING")
