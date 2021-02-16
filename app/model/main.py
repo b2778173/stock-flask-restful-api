26cde4a904598d6467649b932cc693b36fceaa21
Kevin Tsai <b2778173@hotmail.com>
2021-02-17 00:55:08 +0800

    Update main.py

--- app/model/main.py
+++ app/model/main.py
@@ -2,7 +2,7 @@ import mongoengine
 
 
 
-def setup():
+def azure():
     mongoengine.register_connection(
         alias='good',
         db='stock_flask_api_db',
@@ -10,6 +10,18 @@ def setup():
         port=10255,
         username='testapidb',
         password='XDKcnIL9dBEUVPecyyCeTPvdrtsuPMK1PFXQTe6iQx9FaoPzFqAgrWPUl9umCYfU3XStuTGXXBlY1oQyIFy7Mg==',
+        ssl=True,
+        retrywrites=False
+    )
+
+def setup():
+    mongoengine.register_connection(
+        alias='good',
+        db='stock_flask_api_db',
+        host='127.0.0.1',
+        port=27017,
+        username='test',
+        password='test',
         ssl=True,
         retrywrites=False
     )
